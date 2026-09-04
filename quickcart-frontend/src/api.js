fe1cd6e508effe41509100bd214b9af28f3c302a
Sakshi Rawat <sakshirawat964@gmail.com>
2026-09-04 14:55:43 +0530

    Configure production API URL

--- quickcart-frontend/src/api.js
+++ quickcart-frontend/src/api.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:8080/api' });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
